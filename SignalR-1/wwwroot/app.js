b34d93801946351c0064834f5588ce5a766f6e60
Ururu <0668090426fed@gmail.com>
2025-04-14 16:26:16 +0300

    Remove tracked files now ignored

---
+++ SignalR-1/wwwroot/app.js
@@ -0,0 +1,47 @@
+﻿// Создаем connection к нашему хабу ("/chatHub")
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub")  // Адрес нашего хаба
+    .build();
+
+// Функция, которая выводит текст в блок #messagesArea
+function appendMessage(text) {
+    const messagesArea = document.getElementById("messagesArea");
+    const p = document.createElement("p");
+    p.innerText = text;
+    messagesArea.appendChild(p);
+}
+
+// Подписываемся на метод JoinMessage (из интерфейса IChatClient)
+connection.on("JoinMessage", (userId) => {
+    appendMessage(`[JoinMessage] Клиент ${userId} зашел в чат`);
+});
+
+// Подписываемся на метод BroadcastMessage
+connection.on("BroadcastMessage", (userId, message) => {
+    appendMessage(`[BroadcastMessage] ${userId}: ${message}`);
+});
+
+// Запускаем соединение
+connection.start()
+    .then(() => {
+        appendMessage("Соединение установлено!");
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+// Вешаем обработчик на кнопку "Отправить"
+const sendBtn = document.getElementById("sendBtn");
+sendBtn.addEventListener("click", async () => {
+    const messageInput = document.getElementById("messageInput");
+    const message = messageInput.value;
+    if (!message) return;
+
+    try {
+        // Вызываем метод сервера: "SendMessageToAll"
+        await connection.invoke("SendMessageToAll", message);
+        messageInput.value = "";
+    } catch (err) {
+        console.error(err);
+    }
+});
